bc4e6cd017d9b47cd1bf6108f9be8aec33552488
ev0clu <ev0clu@gmail.com>
2023-01-06 21:54:44 +0100

    Basic Layout is prepared with pop up window

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById('btn-add').addEventListener('click', () => {
+    document.getElementById('popup').style.display = 'block';
+});
+
+document.getElementById('btn-close').addEventListener('click', () => {
+    document.getElementById('popup').style.display = 'none';
+});
